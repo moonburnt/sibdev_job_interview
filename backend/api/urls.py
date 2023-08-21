4c5a6563841d39eb306d280bf3ffb52c14679cea
moonburnt <moonburnt@disroot.org>
2023-08-21 11:05:26 +0300

    project template

---
+++ backend/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api import views
+import logging
+
+log = logging.getLogger(__name__)
+
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("auth/", include("rest_framework.urls")),
+]
