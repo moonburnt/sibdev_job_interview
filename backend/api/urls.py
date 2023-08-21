9e23b2719856dab195a1cb366a83bb57fa503f14
moonburnt <moonburnt@disroot.org>
2023-08-21 14:46:58 +0300

    update schema

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from api import views
 import logging
 
 log = logging.getLogger(__name__)
 
-
-router = DefaultRouter()
-router.register("deals", views.DealsViewSet)
-
 urlpatterns = [
-    path("", include(router.urls)),
-    path("auth/", include("rest_framework.urls")),
+    path("", views.DealsViewSet.as_view({"get": "list", "post": "create"})),
 ]
