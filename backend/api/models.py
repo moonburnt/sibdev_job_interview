736c8acade35107b5ce1bf922c85f5f1fb5c05f2
moonburnt <moonburnt@disroot.org>
2023-08-21 11:23:50 +0300

    add models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class ClientModel(models.Model):
+    username = models.CharField(max_length=80)
+    spent_money = models.DecimalField(max_digits=8, decimal_places=2)
+
+class GemModel(models.Model):
+    name = models.CharField(max_length=80)
+    client = models.ManyToManyField(
+        to = ClientModel,
+        related_name= "gems",
+    )
+
+class PurchaseModel(models.Model):
+    gem = models.ForeignKey(
+        to = GemModel,
+        related_name = "purchases",
+        on_delete=models.CASCADE,
+    )
+
+    # I assume, 'total' means "total amount of money spent on purchase"
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField()
+
+    date = models.DateTimeField()
